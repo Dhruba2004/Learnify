7d727f5b0481c4c8aa30f0298958568035228a52
Dhruba2004 <dhrubaduttabanik8@gmail.com>
2024-12-30 11:29:07 +0530

    feat : changes inngest conf

--- app/(routes)/dashboard/_components/Sidebar.jsx
+++ app/(routes)/dashboard/_components/Sidebar.jsx
@@ -1,20 +1,20 @@
-"use client"
+"use client";
 import { CourseCountContext } from "@/app/_context/CourseCountContext";
 import { Button } from "@/app/components/ui/button";
 import { Progress } from "@/app/components/ui/progress";
 import { LayoutDashboard, Shield, UserCircle } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname,useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import React, { useContext } from "react";
 
 function Sidebar() {
-   const {totalCourse,setTotalCourse} = useContext(CourseCountContext)
-   const router = useRouter()
-    const path = usePathname()
-if(totalCourse >= 10 ){
-  router.push('/dashboard/billing')
-}
+  const { totalCourse, setTotalCourse } = useContext(CourseCountContext);
+  const router = useRouter();
+  const path = usePathname();
+  if (totalCourse >= 10) {
+    router.push("/dashboard/billing");
+  }
   const menuList = [
     {
       name: "Dashboard",
@@ -40,23 +40,36 @@ if(totalCourse >= 10 ){
         <h2 className="font-bold text-lg text-white">Easy Study</h2>
       </div>
       <div className="mt-[1rem]">
-        <Link href={'/create'}><Button className="bg-blue-800 w-full text-white"> + Create New</Button></Link>
+        <Link href={"/create"}>
+          <Button className="bg-blue-800 w-full text-white">
+            {" "}
+            + Create New
+          </Button>
+        </Link>
         <div className="mt-5 space-y-3">
           {menuList.map((menu, index) => (
-              <Link className={`flex items-center gap-3 hover:bg-blue-800 rounded-lg p-3 cursor-pointer text-white ${path == menu.path && 'bg-blue-800'}`} key={index} href={menu.path}>
-                <menu.icon />
-                {menu.name}
-              </Link>
+            <Link
+              className={`flex items-center gap-3 hover:bg-blue-800 rounded-lg p-3 cursor-pointer text-white ${
+                path == menu.path && "bg-blue-800"
+              }`}
+              key={index}
+              href={menu.path}
+            >
+              <menu.icon />
+              {menu.name}
+            </Link>
           ))}
         </div>
       </div>
       <div className="p-5 bg-slate-950 rounded-lg mt-5 absolute bottom-10 border w-[87%]">
-        <h2 className="text-lg mb-2 text-white">Available Credits : {10-totalCourse} </h2>
-        <Progress value={(totalCourse/10)*100} />
+        <h2 className="text-lg mb-2 text-white">
+          Available Credits : {10 - totalCourse}{" "}
+        </h2>
+        <Progress value={(totalCourse / 10) * 100} />
         <h2 className="text-sm mt-2">{totalCourse} Out of 10 Credit Used</h2>
-        <Link href={'/dashboard/upgrade'} className="text-xs text-[#5242ef]">Upgrade to create more</Link>
-
-
+        <Link href={"/dashboard/upgrade"} className="text-xs text-[#5242ef]">
+          Upgrade to create more
+        </Link>
       </div>
     </div>
   );
--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,10 +1,12 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../inngest/client";
 import { GenerateFlashCards, GenerateNotes, helloWorld } from "@/app/inngest/functions";
+export const runtime = 'edge'
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
+  streaming:'allow',
   functions: [
     helloWorld,
     GenerateNotes,
