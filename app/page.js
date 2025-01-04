4bfefaade407925732d330ab8dd8106fea27c8fc
Dhruba2004 <dhrubaduttabanik8@gmail.com>
2025-01-04 15:49:24 +0530

    feat : added spotlight in hero section

--- app/page.js
+++ app/page.js
@@ -3,16 +3,14 @@ import Header from "./components/Header";
 import { Button } from "./components/ui/button";
 import {
   Card,
-  CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "./components/ui/card";
 import { Book, Brain, Gamepad, Bot, Loader } from "lucide-react";
 import { TextAnimate } from "./components/ui/text-animate";
-import WordPullUp from "./components/ui/word-pull-up";
 import ShineBorder from "./components/ui/shine-border";
+import { Spotlight } from "./components/ui/spotlight";
 
 export default function Home() {
   const features = [
@@ -47,48 +45,77 @@ export default function Home() {
     <div className="h-screen">
       <section className="h-screen">
         <Header />
-        <div className="relative flex flex-col gap-5 w-full items-center justify-center overflow-hidden rounded-lg p-6 top-[10rem]">
-          <TextAnimate animation="scaleUp" by="text" className="text-center">
-            <span className="pointer-events-none z-10 whitespace-pre-wrap gradient-title text-center text-7xl max-lg:text-[40px] font-bold leading-none tracking-tighter">
-              AI Study Material Generator
-            </span>
-          </TextAnimate>
-          <TextAnimate animation="blurInUp" by="character"className="text-gray-500 max-w-3xl text-center text-2xl max:lg-text-xl">
-            Experience the AI-Powered Learning Revolution and Unlock your exam
-            potential instantly
-          </TextAnimate>
-
-          <div className="flex justify-center items-center gap-5">
-            <Link href="/dashboard">
-              <Button className="text-white max-w-5xl ">Get started</Button>
-            </Link>
-            <Button variant="outline">Watch Video</Button>
+        <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+          <Spotlight
+            className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+            fill="purple"
+          />
+          <Spotlight
+            className="h-[80vh] w-[50vw] top-10 left-full"
+            fill="purple"
+          />
+          <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="purple" />
+          <div className=" p-4 max-w-7xl mx-auto relative z-10  w-full pt-20 md:pt-0">
+            <div className="relative flex flex-col gap-5 w-full items-center justify-center overflow-hidden rounded-lg p-6 max-lg:top-[7rem]">
+              <TextAnimate
+                animation="scaleUp"
+                by="text"
+                className="text-center"
+              >
+                <span className="pointer-events-none z-10 whitespace-pre-wrap gradient-title text-center text-7xl max-lg:text-[40px] font-bold leading-none tracking-tighter">
+                  AI Study Material Generator
+                </span>
+              </TextAnimate>
+              <TextAnimate
+                animation="blurInUp"
+                by="character"
+                className="text-gray-500 max-w-3xl text-center text-2xl max:lg-text-xl"
+              >
+                Experience the AI-Powered Learning Revolution and Unlock your
+                exam potential instantly
+              </TextAnimate>
+              <div className="flex justify-center items-center gap-5">
+                <Link href="/dashboard">
+                <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+                  <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+                  <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-5 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+                    Get Started
+                  </span>
+                </button>
+                </Link>
+                <Button variant="outline">Watch Video</Button>
+               
+              </div>
+            </div>
           </div>
         </div>
       </section>
-      <section className="bg-slate-950 p-20 h-screen overflow-scroll">
+      <section className="bg-slate-950 p-20 overflow-scroll">
         <div className="flex flex-col">
-          <TextAnimate animation="blurInUp" by="character" className="text-5xl font-bold text-center text-blue-500">
+          <TextAnimate
+            animation="blurInUp"
+            by="character"
+            className="text-5xl font-bold text-center text-blue-500"
+          >
             Features We Offer
           </TextAnimate>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-5 mt-[4rem] max-lg:ml-[-1.5rem]">
             {features.map((item, index) => (
-             
-                <Card
-                  key={index}
-                  className="hover:scale-105 transition-all duration-300 ease-in-out border-none "
-                >
-                   <ShineBorder
-                className="relative w-full overflow-hidden rounded-lg border"
-                color={["#A07CFE", "#FE8FB5", "#FFBE7B"]}
+              <Card
+                key={index}
+                className="hover:scale-105 transition-all duration-300 ease-in-out border-none "
               >
+                <ShineBorder
+                  className="relative w-full overflow-hidden rounded-lg border"
+                  color={["#A07CFE", "#FE8FB5", "#FFBE7B"]}
+                >
                   <CardHeader className="flex flex-col items-center justify-center text-center cursor-pointer gap-3">
                     <item.icon className="h-10 w-10" />
                     <CardTitle>{item.title}</CardTitle>
                     <CardDescription>{item.subtitle}</CardDescription>
                   </CardHeader>
-                  </ShineBorder>
-                </Card>
+                </ShineBorder>
+              </Card>
             ))}
           </div>
         </div>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,107 +1,114 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	darkMode: ["class"],
-	content: [
-	  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-	  "./components/**/*.{js,ts,jsx,tsx,mdx}",
-	  "./app/**/*.{js,ts,jsx,tsx,mdx}",
-	],
-	theme: {
-    	extend: {
-    		colors: {
-    			background: 'hsl(var(--background))',
-    			foreground: 'hsl(var(--foreground))',
-    			card: {
-    				DEFAULT: 'hsl(var(--card))',
-    				foreground: 'hsl(var(--card-foreground))'
-    			},
-    			primary: {
-    				DEFAULT: '#5242ef',
-    				foreground: 'hsl(var(--primary-foreground))'
-    			},
-    			secondary: {
-    				DEFAULT: 'hsl(var(--secondary))',
-    				foreground: 'hsl(var(--secondary-foreground))'
-    			},
-    			muted: {
-    				DEFAULT: 'hsl(var(--muted))',
-    				foreground: 'hsl(var(--muted-foreground))'
-    			},
-    			accent: {
-    				DEFAULT: 'hsl(var(--accent))',
-    				foreground: 'hsl(var(--accent-foreground))'
-    			},
-    			destructive: {
-    				DEFAULT: '#FF0000',
-    				foreground: 'hsl(var(--destructive-foreground))'
-    			},
-    			border: 'hsl(var(--border))',
-    			input: 'hsl(var(--input))',
-    			ring: 'hsl(var(--ring))',
-    			chart: {
-    				'1': 'hsl(var(--chart-1))',
-    				'2': 'hsl(var(--chart-2))',
-    				'3': 'hsl(var(--chart-3))',
-    				'4': 'hsl(var(--chart-4))',
-    				'5': 'hsl(var(--chart-5))'
-    			}
-    		},
-    		borderRadius: {
-    			lg: 'var(--radius)',
-    			md: 'calc(var(--radius) - 2px)',
-    			sm: 'calc(var(--radius) - 4px)'
-    		},
-    		typography: {
-    			DEFAULT: {
-    				css: {
-    					h1: {
-    						color: '#BE33FF'
-    					},
-    					h2: {
-    						color: '#BE33FF'
-    					},
-    					h3: {
-    						color: '#808080'
-    					},
-    					h4: {
-    						color: '#3b82f6'
-    					},
-    					h5: {
-    						color: '#3b82f6'
-    					},
-    					h6: {
-    						color: '#3b82f6'
-    					},
-    					p: {
-    						color: '#FFF'
-    					},
-    					ul: {
-    						color: '#808080'
-    					}
-    				}
-    			}
-    		},
-    		animation: {
-    			shine: 'shine var(--duration) infinite linear'
-    		},
-    		keyframes: {
-    			shine: {
-    				'0%': {
-    					'background-position': '0% 0%'
-    				},
-    				'50%': {
-    					'background-position': '100% 100%'
-    				},
-    				to: {
-    					'background-position': '0% 0%'
-    				}
-    			}
-    		}
-    	}
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        primary: {
+          DEFAULT: "#5242ef",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "#FF0000",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          1: "hsl(var(--chart-1))",
+          2: "hsl(var(--chart-2))",
+          3: "hsl(var(--chart-3))",
+          4: "hsl(var(--chart-4))",
+          5: "hsl(var(--chart-5))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            h1: {
+              color: "#BE33FF",
+            },
+            h2: {
+              color: "#BE33FF",
+            },
+            h3: {
+              color: "#808080",
+            },
+            h4: {
+              color: "#3b82f6",
+            },
+            h5: {
+              color: "#3b82f6",
+            },
+            h6: {
+              color: "#3b82f6",
+            },
+            p: {
+              color: "#FFF",
+            },
+            ul: {
+              color: "#808080",
+            },
+          },
+        },
+      },
+      animation: {
+        shine: "shine var(--duration) infinite linear",
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: 0,
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+        shine: {
+          "0%": {
+            "background-position": "0% 0%",
+          },
+          "50%": {
+            "background-position": "100% 100%",
+          },
+          to: {
+            "background-position": "0% 0%",
+          },
+        },
+      },
     },
-	plugins: [
-	  require("tailwindcss-animate"),
-	  require("@tailwindcss/typography"),
-	],
-  };
-  
+  },
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+};
